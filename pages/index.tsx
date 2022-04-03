7846a27573d961d1d4ce5e057e33fce601c79216
Wiktor Musial <wiktormusial@icloud.com>
2022-04-03 21:25:30 +0200

    Create homepage

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,7 +1,17 @@
 import type { NextPage } from "next";
+import Image from "next/image";
 
 const Home: NextPage = () => {
-  return <h1>Test</h1>;
+  return (
+    <div className="text-center mt-5 p-5 md:p-0">
+      <Image
+        src="/albumtemp.JPG"
+        height={500}
+        width={650}
+        alt="Car staying in front of white house"
+      />
+    </div>
+  );
 };
 
 export default Home;
