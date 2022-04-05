adbad404e395b9711d3ed3c14b20fcab3eb46b23
Wiktor Musial <wiktormusial@icloud.com>
2022-04-05 21:20:52 +0200

    Social links

--- pages/contact.tsx
+++ pages/contact.tsx
@@ -8,7 +8,7 @@ const Contact: NextPage = () => {
         <title>Contact</title>
       </Head>
       <h1 className="text-2xl mb-2">Contact</h1>
-      <p>
+      <p className="mb-6">
         Feel free to contact with me via email:{" "}
         <a
           className="text-blue-500 underline"
@@ -17,6 +17,31 @@ const Contact: NextPage = () => {
           wiktormusial@icloud.com
         </a>
       </p>
+      <h1 className="text-2xl mb-2">Social Links</h1>
+      <ul>
+        <li>
+          Twitter{" "}
+          <a
+            className="text-blue-500 underline"
+            href="https://twitter.com/wikmus"
+            target="_blank"
+            rel="noreferrer"
+          >
+            https://twitter.com/wikmus
+          </a>
+        </li>
+        <li>
+          Github{" "}
+          <a
+            className="text-blue-500 underline"
+            href="https://github.com/wiktormusial"
+            target="_blank"
+            rel="noreferrer"
+          >
+            https://github.com/wiktormusial
+          </a>
+        </li>
+      </ul>
     </div>
   );
 };
